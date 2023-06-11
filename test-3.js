e4d097218eec1e6ad0faa5e225eae5034db2eff5
ruscias <stevenwrusciano@gmail.com>
2023-06-11 18:32:05 -0500

    Add front end and js for player selection

--- test-3.js
+++ test-3.js
@@ -7,7 +7,8 @@ const PlayerMaker = (isComputer, playerInt) => {
   const state = {
     isComputer, 
     playerInt,
-    hasWon: false
+    hasWon: false,
+    selectionMade: false
   }
 
   return Object.assign(
@@ -19,8 +20,59 @@ const PlayerMaker = (isComputer, playerInt) => {
 // gameController
 const modalResult = document.querySelector('div#modal-results');
 const modalText = document.querySelector('div#modal-text');
+const modalPlayerSelection = document.querySelector('div#modal-player-selection');
+const playerSelectionButtons = document.querySelectorAll('div.btn-player-selection>button');
+
+const playerOne = PlayerMaker(false,1);
+const playerTwo = PlayerMaker(false,2);
 let turns = 0; 
 let currentPlayer = 'X'; 
+
+function parsePlayerSelectionButtonId(id) {
+  const playerIntMapping = {
+    "one": 1,
+    "two": 2,
+  }
+  const isComputerMapping = {
+    "computer": true,
+    "human": false
+  }
+  const splitId = id.split('-');
+  const result = {playerInt: null, isComputer: null};
+
+  result.playerInt = playerIntMapping[splitId[1]];
+  result.isComputer =  isComputerMapping[splitId[2]]
+  return result;
+}
+
+function recordPlayerSelection() {
+  this.classList.toggle('selected');
+  console.log(`this.classList: ${this.classList}`);
+  const result = parsePlayerSelectionButtonId(this.id);
+  console.log(`result: ${JSON.stringify(result,null,4)}`);
+  if (playerOne.playerInt === result.playerInt) {
+    playerOne.isComputer = result.isComputer;
+    playerOne.selectionMade = true;
+    const buttonsToModify = document.querySelectorAll('div.btn-player-selection>button.btn-player-one');
+    buttonsToModify.forEach( item => {
+      item.removeEventListener('click', recordPlayerSelection);
+      item.disabled = true;
+    })
+  } else if (playerTwo.playerInt === result.playerInt) {
+    playerTwo.isComputer = result.isComputer;
+    playerTwo.selectionMade = true;
+    const buttonsToModify = document.querySelectorAll('div.btn-player-selection>button.btn-player-two');
+    buttonsToModify.forEach( item => {
+      item.removeEventListener('click', recordPlayerSelection);
+      item.disabled = true;
+    })    
+  }
+
+  if (playerOne.selectionMade && playerTwo.selectionMade) {
+    modalPlayerSelection.classList.toggle('hidden');
+  }
+} 
+
 function renderGameboard(gameboard, gameboardItems) {
   for (let i = 0; i < gameboard.length; i++) {
     gameboardItems[i].innerText = gameboard[i];
@@ -110,7 +162,9 @@ function checkForWinner() {
 
 
 
-
+playerSelectionButtons.forEach( item => {
+  item.addEventListener('click', recordPlayerSelection)
+})
 
 gameboardItems.forEach( item => {
   item.addEventListener('click', executeTurn)
